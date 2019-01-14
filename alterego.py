f3968888ba45102293cf092f4520895713103cac
anarkis <>
2019-01-14 23:46:01 +0100

    Now it connects to mongodb, and get the channel to send the hello world message. Also commented some working methods

--- alterego.py
+++ alterego.py
@@ -1,10 +1,16 @@
 from slack.SlackAlterego import SlackAlterego
+from mongodb.MongoDb import MongoDb
 
 if __name__ == '__main__':
     slack = SlackAlterego()
+    mongo = MongoDb()
 
-    result = slack.send_message("XXXXXX", "hello world")
-    #list = slack.get_userlist()
-    #print(slack.get_userchannel(list))
+    # list = slack.get_userlist()
+    # data = slack.get_userchannel(list)
+    # mongo.insert(data)
+    # print("data stored in mongodb")
 
-
+    channel = mongo.get_channel("userxxx")
+    print(channel['channel'])
+    slack.send_message(channel['channel'], "Hello World")
+    print("Fly Butterfly")
